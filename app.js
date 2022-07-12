21d389677847506a04e91a030d2217073b3b3a4e
Hyago Ferreira <hyagosilvaferreira@gmail.com>
2022-07-12 15:38:54 -0300

    Commom Secutity Issues

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const ExpressError = require('./utils/ExpressError');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const userRoutes = require('./routes/users');
 const campgroundsRoutes = require('./routes/campgrounds');
@@ -38,22 +40,83 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
+	name: 'session',
 	secret: 'thisshouldbeabettersecret',
 	resave: false,
 	saveUninitialized: true,
 	// store: mongoStoreSoon
 	cookie: {
-		//for basic security:
-		httpOnly: true,
-
+		httpOnly: true, //for basic security
 		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
 		maxAge: 1000 * 60 * 60 * 24 * 7,
 	},
 };
+
 app.use(session(sessionConfig));
 app.use(flash());
+
+//using helmet
+// app.use(helmet()); //including this breaks the CSP
+
+const scriptSrcUrls = [
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://api.mapbox.com/',
+	'https://kit.fontawesome.com/',
+	'https://cdnjs.cloudflare.com/',
+	'https://cdn.jsdelivr.net/',
+	'https://res.cloudinary.com/dc1grqjs4/',
+	'https://res.cloudinary.com/douqbebwk/',
+];
+const styleSrcUrls = [
+	'https://kit-free.fontawesome.com/',
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.mapbox.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://fonts.googleapis.com/',
+	'https://use.fontawesome.com/',
+	'https://cdn.jsdelivr.net/',
+	'https://res.cloudinary.com/dc1grqjs4/',
+	'https://res.cloudinary.com/douqbebwk/',
+];
+const connectSrcUrls = [
+	'https://*.tiles.mapbox.com',
+	'https://api.mapbox.com',
+	'https://events.mapbox.com',
+	'https://res.cloudinary.com/dc1grqjs4/',
+	'https://res.cloudinary.com/douqbebwk/',
+];
+const fontSrcUrls = ['https://res.cloudinary.com/dc1grqjs4/'];
+
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: ["'self'", ...connectSrcUrls],
+			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+			workerSrc: ["'self'", 'blob:'],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				'blob:',
+				'data:',
+				'https://res.cloudinary.com/dc1grqjs4/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				'https://res.cloudinary.com/douqbebwk/',
+				'https://images.unsplash.com/',
+			],
+			fontSrc: ["'self'", ...fontSrcUrls],
+			mediaSrc: [
+				'https://res.cloudinary.com/dc1grqjs4/',
+				'https://res.cloudinary.com/douqbebwk/',
+			],
+			childSrc: ['blob:'],
+		},
+	})
+);
 
 //session() has to be always before passport.session()
 app.use(passport.initialize());
